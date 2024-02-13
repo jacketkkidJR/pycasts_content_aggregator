22cbc2abff0c076b2cd16a25c453c4a1ecf48549
Timur <tima.gizatullin817@gmail.com>
2024-02-13 14:39:11 +0800

    A couple of minor changes

--- podcasts/views.py
+++ podcasts/views.py
@@ -8,10 +8,10 @@ class PodcastView(LoginRequiredMixin, ListView):
     model = Episode
     template_name = 'pycasts.html'
     login_url = '/login'
+
     def get_context_data(self, **kwargs):
         """Here we override the context data to show only 10 most recent podcasts"""
         context = super().get_context_data(**kwargs)
         context['episodes'] = Episode.objects.filter().order_by("-pub_date")[:10]
         return context
-
 
